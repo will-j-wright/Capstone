998b2fbbb54df1314d445a46bda3a983c3986c14
Will Wright <willjwright@mac.com>
2023-11-29 16:19:18 -0500

    Remove unused dependencies

--- frontend/src/CallbackPage.js
+++ frontend/src/CallbackPage.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { SpotifyToken } from "./App";
 
 const successMessage = 'Successfully loaded Spotify details';
--- frontend/src/History.js
+++ frontend/src/History.js
@@ -3,7 +3,6 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from "react-bootstrap/Button";
 import { useEffect, useState } from "react";
 import SongTable from './SongTable';
 
